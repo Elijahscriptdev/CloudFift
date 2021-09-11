c973e05d4a99e2d333f4fcaf4cc12f9462ef0dce
Elijah Obominuru <elijahobominuru@gmail.com>
2021-09-11 15:02:18 +0100

    clean up files, setup project structure by creating components folder and add sass for styling

---
+++ src/components/header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>logo</li>
+            </ul>
+
+            <ul>
+                <li>Companies</li>
+                <li>Candidates</li>
+                <li>Assessment</li>
+                <li>Post a Job</li>
+                <li>Career  Advice</li>
+            </ul>
+
+            <ul>
+                <li>Sign Up</li>
+                <li>Log In</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './hero.scss'
+
+const Hero = () => {
+    return (
+        <div className='hero__wrapper'>
+            Hello
+        </div>
+    )
+}
+
+export default Hero;
