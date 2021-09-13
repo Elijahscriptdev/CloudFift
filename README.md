0c0c2bf37402304e69faaa6c0110509cbdaa2a82
Elijah Obominuru <elijahobominuru@gmail.com>
2021-09-13 09:36:37 +0100

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 
+# CloudFit Task
+
+Implement the design with code to be almost as close as possible. Show us your clean architecture and project structure scalability.
+
+What we are looking for:
+Production grade code (clean, maintainable, reusable code)
+A detailed commit history shows your progress towards your final implementation.
+Good sense of design
+Responsive Design
+
+If your aim is to build something awesome, you have a tribe here!
+https://www.figma.com/file/s3nZ9JdH3dV5wAQJmzmBih/?node-id=0%3A1
+
+## Built With
+
+- React
+- React Hooks
+- Netlify (For deployment)
+- ES6 JavaScript.
+
+## Deployment to Netlify
+[Live link](https://cloudfitjobtask.netlify.app/)
+
+## Images
+
+![homepage](https://res.cloudinary.com/elijjaaahhhh/image/upload/v1631522069/FireShot_Capture_317_-_Task_-_cloudfitjobtask.netlify.app_x1s8bx.png)
+
+![mobile](https://res.cloudinary.com/elijjaaahhhh/image/upload/v1631522166/FireShot_Capture_318_-_Task_-_localhost_jaypiz.png)
+
+## Prerequisites
+
+- React
+
+## Setup
+
+```sh
+git clone https://github.com/Elijahscriptdev/CloudFift.git
+```
+
+Navigate to the extracted folder
+
+```sh
+cd CloudFift
+```
+
+Install dependencies
+
+```sh
+yarn
+```
+
+Start server with:
+
+```sh
+    yarn start
+```
+
+Open `http://localhost:3000/` in your browser.
+
+## Author
+
+👤 **Elijah Obominuru**
+
+[Github](https://github.com/Elijahscriptdev) | [Twitter](https://twitter.com/ElijahObominuru) | [LinkedIn](https://www.linkedin.com/in/elijah-obominuru-0b730b143/)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is licensed under MIT license - see [LICENSE](/LICENSE) for more details.
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import "./Header.scss";
 import LOGO from "../assets/images/logo.svg";
-import { FiMenu } from 'react-icons/fi';
+import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
-
   const [click, setClick] = useState(false);
 
   const handleClick = () => {
@@ -12,7 +11,6 @@ const Header = () => {
   };
 
   const closeMobileMenu = () => setClick(false);
-
 
   return (
     <div className='header__wrapper'>
@@ -39,25 +37,28 @@ const Header = () => {
         </ul>
       </div>
 
-      <div className="mobile-nav">
+      <div className='mobile-nav'>
         <ul>
-          <li><FiMenu /></li>
+          <li>
+            <FiMenu />
+          </li>
         </ul>
-        {click && <div className='nav-link'>
-        <ul className='nav-links'>
-          <li className='nav-items'>Companies</li>
-          <li className='nav-items'>Candidates</li>
-          <li className='nav-items'>Assessment</li>
-          <li className='nav-items'>Post a Job</li>
-          <li className='nav-items'>Career Advice</li>
-        </ul>
+        {click && (
+          <div className='nav-link'>
+            <ul className='nav-links'>
+              <li className='nav-items'>Companies</li>
+              <li className='nav-items'>Candidates</li>
+              <li className='nav-items'>Assessment</li>
+              <li className='nav-items'>Post a Job</li>
+              <li className='nav-items'>Career Advice</li>
+            </ul>
 
-        <ul className='nav-links'>
-          <li className='auth-links'>Sign Up</li>
-          <li className='auth-links login'>Log In</li>
-        </ul>
-      </div>}
-
+            <ul className='nav-links'>
+              <li className='auth-links'>Sign Up</li>
+              <li className='auth-links login'>Log In</li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
